a06cfe52f19c8b4a794a304b71d09a22b3faee97
Bertrand C <bco@wflo.fr>
2023-01-11 15:21:51 +0100

    Commit from TeleportHQ

---
+++ components/footer.js
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const Footer = (props) => {
+  return (
+    <>
+      <footer className="footer-footer section-container">
+        <div className="footer-max-width max-content-container">
+          <div className="footer-container">
+            <span className="footer-text">
+              <span>Du lundi au vendredi :</span>
+              <br></br>
+              <span> 9h30 – 12h30</span>
+              <br></br>
+              <span> 14h00 – 17h30</span>
+            </span>
+            <div className="footer-social-media">
+              <span className="footer-text6">Suivez nous :</span>
+              <a
+                href="https://www.facebook.com/AtelierInformatiqueMedia/"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="footer-link"
+              >
+                <div className="footer-container1">
+                  <svg
+                    viewBox="0 0 877.7142857142857 1024"
+                    className="footer-icon"
+                  >
+                    <path d="M829.143 73.143c26.857 0 48.571 21.714 48.571 48.571v780.571c0 26.857-21.714 48.571-48.571 48.571h-223.429v-340h113.714l17.143-132.571h-130.857v-84.571c0-38.286 10.286-64 65.714-64l69.714-0.571v-118.286c-12-1.714-53.714-5.143-101.714-5.143-101.143 0-170.857 61.714-170.857 174.857v97.714h-114.286v132.571h114.286v340h-420c-26.857 0-48.571-21.714-48.571-48.571v-780.571c0-26.857 21.714-48.571 48.571-48.571h780.571z"></path>
+                  </svg>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <style jsx>
+        {`
+          .footer-footer {
+            border-color: rgba(33, 33, 33, 0.1);
+            border-width: 1px;
+            border-top-width: 0px;
+            border-left-width: 0px;
+            border-right-width: 0px;
+          }
+          .footer-max-width {
+            align-self: center;
+            justify-content: center;
+          }
+          .footer-container {
+            flex: 0 0 auto;
+            width: 50%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+          }
+          .footer-text {
+            color: var(--dl-color-grays-gray100);
+            max-width: 350px;
+            margin-bottom: 60px;
+          }
+          .footer-social-media {
+            flex: 0 0 auto;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+          .footer-text6 {
+            font-size: 18px;
+            font-style: normal;
+            font-weight: 600;
+            margin-bottom: var(--dl-space-space-oneandhalfunits);
+          }
+          .footer-link {
+            display: contents;
+          }
+          .footer-container1 {
+            flex: 0 0 auto;
+            display: flex;
+            align-self: center;
+            align-items: flex-start;
+            flex-direction: row;
+            justify-content: flex-start;
+            text-decoration: none;
+          }
+          .footer-icon {
+            width: 24px;
+            height: 24px;
+          }
+          @media (max-width: 991px) {
+            .footer-max-width {
+              flex-direction: column-reverse;
+            }
+            .footer-container {
+              width: 100%;
+            }
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,303 @@
+import React from 'react'
+import Head from 'next/head'
+
+import Footer from '../components/footer'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>atelier-im</title>
+          <meta property="og:title" content="atelier-im" />
+        </Head>
+        <main className="home-main">
+          <div className="section-container">
+            <div className="home-max-width max-content-container">
+              <div className="home-image-container">
+                <img
+                  alt="image"
+                  src="/playground_assets/logo_facebook.png"
+                  className="home-image"
+                />
+              </div>
+              <div className="home-content-container">
+                <h1 className="home-text">
+                  <span className="home-text01">6 Rue des écoles</span>
+                  <br></br>
+                  <span>07190 Saint Sauveur de Montagut</span>
+                  <br></br>
+                </h1>
+                <div className="home-input-container">
+                  <div className="home-container1 input">
+                    <svg
+                      viewBox="0 0 804.5714285714286 1024"
+                      className="home-icon"
+                    >
+                      <path d="M804.571 708.571c0 20.571-9.143 60.571-17.714 79.429-12 28-44 46.286-69.714 60.571-33.714 18.286-68 29.143-106.286 29.143-53.143 0-101.143-21.714-149.714-39.429-34.857-12.571-68.571-28-100-47.429-97.143-60-214.286-177.143-274.286-274.286-19.429-31.429-34.857-65.143-47.429-100-17.714-48.571-39.429-96.571-39.429-149.714 0-38.286 10.857-72.571 29.143-106.286 14.286-25.714 32.571-57.714 60.571-69.714 18.857-8.571 58.857-17.714 79.429-17.714 4 0 8 0 12 1.714 12 4 24.571 32 30.286 43.429 18.286 32.571 36 65.714 54.857 97.714 9.143 14.857 26.286 33.143 26.286 50.857 0 34.857-103.429 85.714-103.429 116.571 0 15.429 14.286 35.429 22.286 49.143 57.714 104 129.714 176 233.714 233.714 13.714 8 33.714 22.286 49.143 22.286 30.857 0 81.714-103.429 116.571-103.429 17.714 0 36 17.143 50.857 26.286 32 18.857 65.143 36.571 97.714 54.857 11.429 5.714 39.429 18.286 43.429 30.286 1.714 4 1.714 8 1.714 12z"></path>
+                    </svg>
+                    <span className="home-text05">06 14 82 85 70</span>
+                  </div>
+                  <div className="home-container2 input">
+                    <svg
+                      viewBox="0 0 804.5714285714286 1024"
+                      className="home-icon02"
+                    >
+                      <path d="M804.571 708.571c0 20.571-9.143 60.571-17.714 79.429-12 28-44 46.286-69.714 60.571-33.714 18.286-68 29.143-106.286 29.143-53.143 0-101.143-21.714-149.714-39.429-34.857-12.571-68.571-28-100-47.429-97.143-60-214.286-177.143-274.286-274.286-19.429-31.429-34.857-65.143-47.429-100-17.714-48.571-39.429-96.571-39.429-149.714 0-38.286 10.857-72.571 29.143-106.286 14.286-25.714 32.571-57.714 60.571-69.714 18.857-8.571 58.857-17.714 79.429-17.714 4 0 8 0 12 1.714 12 4 24.571 32 30.286 43.429 18.286 32.571 36 65.714 54.857 97.714 9.143 14.857 26.286 33.143 26.286 50.857 0 34.857-103.429 85.714-103.429 116.571 0 15.429 14.286 35.429 22.286 49.143 57.714 104 129.714 176 233.714 233.714 13.714 8 33.714 22.286 49.143 22.286 30.857 0 81.714-103.429 116.571-103.429 17.714 0 36 17.143 50.857 26.286 32 18.857 65.143 36.571 97.714 54.857 11.429 5.714 39.429 18.286 43.429 30.286 1.714 4 1.714 8 1.714 12z"></path>
+                    </svg>
+                    <span className="home-text06">admin@atelier-im.com</span>
+                  </div>
+                </div>
+                <div className="home-features-container">
+                  <div className="home-feature">
+                    <svg
+                      viewBox="0 0 877.7142857142857 1024"
+                      className="home-icon04"
+                    >
+                      <path d="M733.714 419.429c0-9.714-3.429-19.429-10.286-26.286l-52-51.429c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-233.143 232.571-129.143-129.143c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-52 51.429c-6.857 6.857-10.286 16.571-10.286 26.286s3.429 18.857 10.286 25.714l206.857 206.857c6.857 6.857 16.571 10.857 25.714 10.857 9.714 0 19.429-4 26.286-10.857l310.286-310.286c6.857-6.857 10.286-16 10.286-25.714zM877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+                    </svg>
+                    <span className="home-text07">
+                      Dépannage, Installation, Configuration, Vente en
+                      Informatique et Réseaux
+                    </span>
+                  </div>
+                  <div className="home-feature1">
+                    <svg
+                      viewBox="0 0 877.7142857142857 1024"
+                      className="home-icon06"
+                    >
+                      <path d="M733.714 419.429c0-9.714-3.429-19.429-10.286-26.286l-52-51.429c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-233.143 232.571-129.143-129.143c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-52 51.429c-6.857 6.857-10.286 16.571-10.286 26.286s3.429 18.857 10.286 25.714l206.857 206.857c6.857 6.857 16.571 10.857 25.714 10.857 9.714 0 19.429-4 26.286-10.857l310.286-310.286c6.857-6.857 10.286-16 10.286-25.714zM877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+                    </svg>
+                    <span className="home-text08">Tous travaux media</span>
+                  </div>
+                  <div className="home-feature2">
+                    <svg
+                      viewBox="0 0 877.7142857142857 1024"
+                      className="home-icon08"
+                    >
+                      <path d="M733.714 419.429c0-9.714-3.429-19.429-10.286-26.286l-52-51.429c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-233.143 232.571-129.143-129.143c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-52 51.429c-6.857 6.857-10.286 16.571-10.286 26.286s3.429 18.857 10.286 25.714l206.857 206.857c6.857 6.857 16.571 10.857 25.714 10.857 9.714 0 19.429-4 26.286-10.857l310.286-310.286c6.857-6.857 10.286-16 10.286-25.714zM877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+                    </svg>
+                    <span className="home-text09">
+                      Sites web, boutiques en ligne
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer></Footer>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            position: relative;
+            min-height: 100vh;
+            overflow-x: hidden;
+            align-items: center;
+            flex-direction: column;
+            justify-content: flex-start;
+            background-color: var(--dl-color-backgrounds-gray);
+          }
+          .home-main {
+            flex: 0 0 auto;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-max-width {
+            align-items: stretch;
+          }
+          .home-image-container {
+            flex: 1;
+            width: 100%;
+            display: flex;
+            align-self: center;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .home-image {
+            flex: 1;
+            width: 468px;
+            height: 476px;
+            object-fit: cover;
+          }
+          .home-content-container {
+            flex: 1;
+            display: flex;
+            min-width: 50%;
+            align-items: flex-start;
+            padding-right: var(--dl-space-space-twounits);
+            flex-direction: column;
+            justify-content: center;
+          }
+          .home-text {
+            width: 90%;
+            font-size: 30px;
+            margin-bottom: 42px;
+          }
+          .home-input-container {
+            flex: 0 0 auto;
+            width: 100%;
+            display: flex;
+            margin-bottom: var(--dl-space-space-fourunits);
+            flex-direction: row;
+          }
+          .home-container1 {
+            width: 100%;
+            height: 56px;
+            display: flex;
+            max-width: 376px;
+            align-items: center;
+            line-height: 1.5;
+            padding-top: var(--dl-space-space-unit);
+            border-width: 0px;
+            margin-right: var(--dl-space-space-halfunit);
+            padding-left: var(--dl-space-space-oneandhalfunits);
+            border-radius: 32px;
+            padding-right: var(--dl-space-space-oneandhalfunits);
+            padding-bottom: var(--dl-space-space-unit);
+            background-color: var(--dl-color-backgrounds-gray);
+          }
+          .home-icon {
+            fill: var(--dl-color-grays-gray40);
+            width: 20px;
+            height: 20px;
+          }
+          .home-text05 {
+            color: var(--dl-color-grays-gray60);
+            background-color: var(--dl-color-grays-white100);
+          }
+          .home-container2 {
+            width: 100%;
+            height: 56px;
+            display: flex;
+            max-width: 376px;
+            align-items: center;
+            line-height: 1.5;
+            padding-top: var(--dl-space-space-unit);
+            border-width: 0px;
+            margin-right: var(--dl-space-space-halfunit);
+            padding-left: var(--dl-space-space-oneandhalfunits);
+            border-radius: 32px;
+            padding-right: var(--dl-space-space-oneandhalfunits);
+            padding-bottom: var(--dl-space-space-unit);
+            background-color: var(--dl-color-backgrounds-gray);
+          }
+          .home-icon02 {
+            fill: var(--dl-color-grays-gray40);
+            width: 20px;
+            height: 20px;
+          }
+          .home-text06 {
+            color: var(--dl-color-grays-gray60);
+            background-color: var(--dl-color-backgrounds-gray);
+          }
+          .home-features-container {
+            flex: 0 0 auto;
+            width: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+          .home-feature {
+            flex: 0 0 auto;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+            flex-direction: row;
+          }
+          .home-icon04 {
+            fill: #62bf77;
+            width: 17px;
+            height: 17px;
+            min-width: 17px;
+            margin-right: 12px;
+          }
+          .home-text07 {
+            color: var(--dl-color-grays-gray60);
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 400;
+          }
+          .home-feature1 {
+            flex: 0 0 auto;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+            flex-direction: row;
+          }
+          .home-icon06 {
+            fill: #62bf77;
+            width: 17px;
+            height: 17px;
+            min-width: 17px;
+            margin-right: 12px;
+          }
+          .home-text08 {
+            color: var(--dl-color-grays-gray60);
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 400;
+          }
+          .home-feature2 {
+            flex: 0 0 auto;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+          }
+          .home-icon08 {
+            fill: #62bf77;
+            width: 17px;
+            height: 17px;
+            min-width: 17px;
+            margin-right: 12px;
+          }
+          .home-text09 {
+            color: var(--dl-color-grays-gray60);
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 400;
+          }
+          @media (max-width: 991px) {
+            .home-max-width {
+              flex-direction: column;
+            }
+            .home-content-container {
+              margin-bottom: 42px;
+              padding-right: 0px;
+            }
+            .home-text {
+              width: 100%;
+            }
+            .home-text01 {
+              width: 100%;
+            }
+          }
+          @media (max-width: 479px) {
+            .home-input-container {
+              flex-direction: column;
+            }
+            .home-container1 {
+              max-width: 100%;
+              margin-bottom: var(--dl-space-space-unit);
+            }
+            .home-container2 {
+              max-width: 100%;
+              margin-bottom: var(--dl-space-space-unit);
+            }
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
